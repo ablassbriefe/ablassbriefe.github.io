7169868d93125dc0a873affa4ef9c9e6e0eefbea
Syr0kx <sven-spieler@web.de>
2016-06-28 14:47:59 +0200

    lol
    lol

---
+++ bought.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Hiermit erlasse ich dir <?php echo $_GET["name"]; ?><br> deine
+bereute Tat: <?php echo $_GET["bereutetat"]; ?>. Überdenke deine nächsten Taten.
+
+</body>
+</html> 
